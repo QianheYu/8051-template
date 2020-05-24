235205fe4fb2fdbb49935a405c72ba9fb2b79767
Qianhe <qianhegit@users.noreply.github.com>
2020-05-24 12:33:52 +0800

    create serial-example
    串口通信示例代码
    使用P1^0处的开关控制显示

--- Example/serial/serial-mod1.c
+++ Example/serial/serial-mod1.c
@@ -1,32 +1,89 @@
 #include "REG52.h"
 #define WORD P2
+#define KEY P1^0
+/**
+ * Define the function about Timer, Interrupt and Serial
+ * to make them looks great more.
+*/
+#define Enable_Interrupt(X) (X = 1)
+#define Disable_Interrupt(X) (X = 0)
 
-code unsigned char words[10] = {0xc0,0xf6,0xa4,0xb0,0x98,0x92,0x82,0xf8,0x80,0x90};  //use 7SEG-COM-ANODE
+#ifdef __Enable_Priority__			//use the priority on your needs
+	#define SetPriority(X) (IP = X)
+#endif
 
-void serialInit(){
+#define Enable_Timer(X) (X = 1)
+#define Disable_Timer(X) (X = 0)
 
+#define SetTimermode(X) (TMOD |= X)
+#define ResetTimermode() (TMOD = 0x00)
+
+#define SetSerial(X) (SCON = X)
+#define Enable_MultFrequency() (SMOD = 1)
+#define Disable_MultFrequnency() (SMOD = 0)
+#define Enable_Reseive() (REN = 1)
+#define Disable_Reseive() (REN = 0)
+#define GetState(X) (X)
+
+code unsigned char words[10] = {0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f};  //use 7SEG-COM-ANODE
+
+/**
+ * @function Init Interrupt
+*/
+void InterruptInit(){
+	
+#ifdef __Enable_Priority__
+	SetPriority();
+#endif
+	
+	Enable_Interrupt(ES);
+	Enable_Interrupt(EA);
 }
 
-void keyScan(){
-
+void delay(unsigned int time){
+	while(time--);
 }
 
-void show(int tab){
-    WORD = words[tab];
+/**
+ * @function Init Timer.
+*/
+void TimerInit(){
+    SetTimermode(0x20);
+    TH1 = 0xfd;
+    TL1 = 0xfd;
 }
 
 void main(){
-
+    int i = 0;
+    InterruptInit();
+    TimerInit();
+    SetSerial(0x50);
+    Enable_Timer(TR1);
     while (1)
     {
-        /* code */
+        switch(~KEY){
+            case 1:
+                SBUF = ~words[i++];
+                break;
+            default:
+                SBUF = 0xff;
+        }
+		while(!TI){
+			TI = 0;
+		}
+        if(i > 9){
+            i = 0;
+        }
+		delay(10000);
+		
     }
     
 }
 
 void serialHandler() interrupt 4
 {
-    if(mode){
-        show(i++);
-    }
+    if(GetState(RI)){
+		P2 = SBUF;
+    } 
+	RI = 0;
 }
